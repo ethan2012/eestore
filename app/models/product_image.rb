b6623cc90007348383ec8e2f5488d749f43c97f4
yuchen wang <ywang@alwaysreach.net>
2017-06-06 06:39:56 -0400

    only save original images with 200*200

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,9 +1,7 @@
 class ProductImage < ApplicationRecord
   belongs_to :product
   has_attached_file :avatar, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
+    original: '200x200#',
   }
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
